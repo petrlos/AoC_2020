33ec5c4ac8315374fc8eb93835e8a8c540832aed
Petr Los <petrlos@gmail.com>
2022-01-10 00:08:44 +0100

    Day 24: Star 2 working

--- 23/23_part2.py
+++ 23/23_part2.py
@@ -1,4 +1,6 @@
 #Advent of Code 2020: Day 23
+from datetime import datetime
+startTime = datetime.now()
 
 def letsPlayAGameTask1(rounds):
     currentCup = int(input[0])
@@ -20,6 +22,26 @@ def letsPlayAGameTask1(rounds):
         result += str(next)
     return result
 
+def letsPlayAGameTask2(rounds):
+    currentCup = int(input[0])
+    for moves in range(rounds):
+        triplet = [ct[currentCup], ct[ct[currentCup]], ct[ct[ct[currentCup]]]]
+        destCup = currentCup - 1
+        if destCup == 0:
+            destCup = 1000000
+        while destCup in triplet:
+            destCup -= 1
+            if destCup == 0:
+                destCup = 1000000
+        ct[triplet[-1]], ct[destCup], ct[currentCup] = ct[destCup], triplet[0], ct[triplet[-1]]
+        currentCup = ct[currentCup]
+    next = 1
+    result = 1
+    for _ in range(2):
+        next = ct[next]
+        result *= next
+    return result
+
 #MAIN
 input = "156794823"
 
@@ -27,5 +49,16 @@ ct = dict() #crabTable
 #startsequence
 for index in range(len(input)):
     ct[int(input[index])] = int(input[(index + 1) % len(input)])
-
 print("Task 1:",letsPlayAGameTask1(100))
+
+ct = dict() #crabTable
+#startsequence
+for index in range(len(input)):
+    ct[int(input[index])] = int(input[(index + 1) % len(input)])
+for i in range(10,1000001):
+    ct[i] = i+1
+ct[int(input[-1])] = 10 #last number in input points to 10
+ct[1000000] = int(input[0]) #cup 1.000.000 points to first cup
+print("Task 2:", letsPlayAGameTask2(10000000))
+
+print("Runtime:", datetime.now() - startTime)
